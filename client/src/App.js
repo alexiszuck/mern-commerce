f4cb8dddb8be8163befdf941e130ee1e6ead2e6f
alexisZuck <alexiszuck@reddithub.com>
2022-05-11 01:55:43 +0000

    hello, world

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="">
+      <header className="flex bg-slate-800 w-screen h-screen">
+        <h1 className="mx-auto mt-8 text-3xl font-bold underline text-white">
+          Hello world!
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
